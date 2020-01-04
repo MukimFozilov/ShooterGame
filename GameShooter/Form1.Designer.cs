d67950620611e17d0bb108a27a4335739739b0cd
MukimFozilov <mukim.fozilov@yandex.ru>
2020-01-04 16:12:29 +0300

    ShooterGame

---
+++ GameShooter/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace GameShooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.lb_score = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::GameShooter.Properties.Resources.jet_plane_2;
+            this.pictureBox4.Location = new System.Drawing.Point(533, 260);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::GameShooter.Properties.Resources.bullet;
+            this.pictureBox3.Location = new System.Drawing.Point(132, 381);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(19, 19);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GameShooter.Properties.Resources.bullet;
+            this.pictureBox2.Location = new System.Drawing.Point(132, 350);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(19, 19);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GameShooter.Properties.Resources.jet_plane_1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 350);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 200;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 10;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // lb_score
+            // 
+            this.lb_score.AutoSize = true;
+            this.lb_score.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_score.ForeColor = System.Drawing.Color.White;
+            this.lb_score.Location = new System.Drawing.Point(13, 13);
+            this.lb_score.Name = "lb_score";
+            this.lb_score.Size = new System.Drawing.Size(0, 32);
+            this.lb_score.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(400, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 32);
+            this.label1.TabIndex = 5;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::GameShooter.Properties.Resources.jet_plane_2;
+            this.pictureBox5.Location = new System.Drawing.Point(731, 491);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::GameShooter.Properties.Resources.jet_plane_2;
+            this.pictureBox6.Location = new System.Drawing.Point(761, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_score);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label lb_score;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ GameShooter/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameShooter
+{
+    public partial class Form1 : Form
+    {
+        int bulletcount = 0;
+        int score = 0;
+        Random ran = new Random();
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            lb_score.Enabled = false;
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox2.Enabled = true;
+            pictureBox2.Left += 10;
+          
+             
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox3.Enabled = true;
+            pictureBox3.Left += 10;
+        }
+
+        private void Random_Plan(int speed)
+        {
+            Random r = new Random();
+
+            int x1, y1, x2, y2, x3, y3;
+            if (pictureBox4.Left >= 0 || pictureBox5.Left >= 0 || pictureBox6.Left >= 0)
+            {
+                pictureBox4.Left += -speed;
+                pictureBox5.Left += -speed;
+                pictureBox6.Left += -speed;
+             
+            }
+            else
+            {
+                x1 = r.Next(0, 900);
+                y1 = r.Next(0, 600);
+                x2 = r.Next(0, 800);
+                y2 = r.Next(0, 500);
+                x3 = r.Next(0, 950);
+                y3 = r.Next(0, 550);
+
+                pictureBox4.Location = new Point(x1, y1);
+                pictureBox5.Location = new Point(x2, y2);
+                pictureBox6.Location = new Point(x3, y3);  
+                
+            }
+        }
+
+        private void GameOver()
+        {
+            if(pictureBox4.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                timer3.Enabled = false;
+                label1.Text = "Game over";
+            }
+        }
+
+        private void Shooter_Plane()
+        {
+            if (pictureBox4.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                pictureBox4.Location = new Point(ran.Next(100, 800), ran.Next(300, 600));
+                score++;
+                lb_score.Text = "Score = "+score.ToString();
+            }
+            
+        }
+
+        private void Restart()
+        {
+            Application.Restart();
+        }
+
+     
+      
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           if(e.KeyCode == Keys.Down)
+           {
+               if(pictureBox1.Top <900)
+               {
+                   pictureBox1.Top += 10;
+               }
+               
+           }
+            else if(e.KeyCode == Keys.Up)
+           {
+                if(pictureBox1.Top > 10)
+                {
+                    pictureBox1.Top -=10;
+                }    
+           }
+           else if(e.KeyCode == Keys.Space)
+           {
+               if(bulletcount % 1 == 0)
+               {
+                   pictureBox2.Location = pictureBox1.Location;
+                   bulletcount++;
+               }
+         
+           }
+            else if (e.KeyCode == Keys.R)
+           {
+               Restart();
+           }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Random_Plan(100);
+          
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            Shooter_Plane();
+            GameOver();
+           
+        }
+
+       
+    }
+}
